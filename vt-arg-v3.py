d266094214fc51786081b57acdb8653a69354546
BIitzkrieg <andrew.danis@outlook.com>
2022-12-28 14:15:09 -0500

    Update vt-arg-v3.py

--- vt-arg-v3.py
+++ vt-arg-v3.py
@@ -1,5 +1,6 @@
-import re
 import virustotal3.core
+import re
+import sys
 from datetime import *
 
 API_KEY = ''
@@ -12,36 +13,43 @@ premium_api = True
 def seperator():
     print("========================================================================")
 
-def vtMain():
-    f = open("ioc.txt")
-    contents = f.read()
-    file_as_list = list(dict.fromkeys((contents.splitlines())))
+def vtMain(iocList):
     count = 0
-    for ioc in file_as_list:
+    for ioc in iocList:
         count += 1
         if premium_api == True:
-            # print("Premium API set... We will use up to the limit you set in the script based on your subscription")
+            #print("Premium API set... We will use up to the limit you set in the script based on your subscription")
             if count == 1000:
                 print("Hit Premium API limit.. exiting")
                 break
             vtLogic(ioc, count)
 
+        elif premium_api == False:
+            #print("Regular API set... Script will wait 60 seconds every 4 IOC's")
+            if count == 4:
+                count = 0
+                print("Sleeping, waiting for VT API", time.sleep(60))
+                continue
+            vtLogic(ioc, count)
+
+
 def vtLogic(ioc, count):
-    # MD5
-    if re.match(r"(^[a-fA-F\d]{32}$)", str(ioc)):
+    # Hash
+    if re.match(r"(^[a-fA-F\d]{32}$)|(^[a-fA-F\d]{40}$)|(^[a-fA-F\d]{64}$)", str(ioc)):
         try:
             results = vt_files.info_file(ioc)
-            #pprint.pprint(results)
+            # pprint.pprint(results)
             seperator()
 
             if results['data']:
                 count += 1
-                print("MD5: " + str(ioc))
+                print("Hash: " + str(ioc))
                 print()
-                #print(json.dumps(results, indent=4,sort_keys=True))
+                # print(json.dumps(results, indent=4,sort_keys=True))
                 last_scan = results['data']['attributes']['last_analysis_date']
                 local_time = datetime.fromtimestamp(last_scan).strftime('%c')
                 print("Last Scan: " + local_time + " EST")
+                print("Equivalent MD5: " + results['data']['attributes']['md5'])
                 print("Equivalent SHA-1: " + results['data']['attributes']['sha1'])
                 print("Equivalent SHA-256: " + results['data']['attributes']['sha256'])
                 num_malicious = int(results['data']['attributes']['last_analysis_stats']['malicious'])
@@ -49,54 +57,7 @@ def vtLogic(ioc, count):
                 total = num_malicious + num_undetected
                 print("VT Results:", num_malicious, '\\', total)
         except:
-            raise
-    # SHA-1
-    elif re.match(r"(^[a-fA-F\d]{40}$)", str(ioc)):
-        try:
-            results = vt_files.info_file(ioc)
-            #pprint.pprint(results)
-            seperator()
-
-            if results['data']:
-                count += 1
-                print("SHA-1: " + str(ioc))
-                print()
-                #print(json.dumps(results, indent=4,sort_keys=True))
-                last_scan = results['data']['attributes']['last_analysis_date']
-                local_time = datetime.fromtimestamp(last_scan).strftime('%c')
-                print("Last Scan: " + local_time + " EST")
-                print("Equivalent MD5: " + results['data']['attributes']['md5'])
-                print("Equivalent SHA-256: " + results['data']['attributes']['sha256'])
-                num_malicious = int(results['data']['attributes']['last_analysis_stats']['malicious'])
-                num_undetected = int(results['data']['attributes']['last_analysis_stats']['undetected'])
-                total = num_malicious + num_undetected
-                print("VT Results:", num_malicious, '\\', total)
-        except:
             pass
-    # SHA-256
-    elif re.match(r"(^[a-fA-F\d]{64}$)", str(ioc)):
-        try:
-            results = vt_files.info_file(ioc)
-            #pprint.pprint(results)
-            seperator()
-
-            if results['data']:
-                count += 1
-                print("SHA-1: " + str(ioc))
-                print()
-                #print(json.dumps(results, indent=4,sort_keys=True))
-                last_scan = results['data']['attributes']['last_analysis_date']
-                local_time = datetime.fromtimestamp(last_scan).strftime('%c')
-                print("Last Scan: " + local_time + " EST")
-                print("Equivalent MD5: " + results['data']['attributes']['md5'])
-                print("Equivalent SHA-1: " + results['data']['attributes']['sha1'])
-                num_malicious = int(results['data']['attributes']['last_analysis_stats']['malicious'])
-                num_undetected = int(results['data']['attributes']['last_analysis_stats']['undetected'])
-                total = num_malicious + num_undetected
-                print("VT Results:", num_malicious, '\\', total)
-        except:
-            pass
-
 
     # Domain
     elif re.match(r"^(?!:\/\/)([a-zA-Z0-9-_]+\.)*[a-zA-Z0-9][a-zA-Z0-9-_]+\.[a-zA-Z]{2,11}?$", str(ioc)):
@@ -114,7 +75,9 @@ def vtLogic(ioc, count):
         except:
             pass
     # URL
-    elif re.match(r"(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})", str(ioc)):
+    elif re.match(
+            r"(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})",
+            str(ioc)):
         try:
             results = vt_URL.info_url(ioc)
             seperator()
@@ -143,8 +106,9 @@ def vtLogic(ioc, count):
             pass
 
 
-def main():
-    vtMain()
+def main(iocList):
+    vtMain(iocList)
 
 if __name__ == '__main__':
-    main()
+    iocList = list(dict.fromkeys(sys.argv[1:]))
+    main(iocList)
